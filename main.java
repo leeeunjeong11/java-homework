c49f1b4f2bdf2d503b48c4c6712261b168b3a318
leeeunjeong11 <fsgjr34342gmail.com>
2025-09-16 19:49:02 +0900

    첫번째 commit: 더하기, 빼기 수행

---
+++ main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("덧셈: " + (a + b));
+        System.out.println("뺄셈: " + (a - b));
+    }
+}
