ac24fce563bfbf5bdfa75171cdc4765356cc7851
leeeunjeong11 <fsgjr34342gmail.com>
2025-09-16 19:51:50 +0900

    두번째 commit: 곱하기, 나누기 수행

--- main.java
+++ main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         int a = 10;
-        int b = 5;
+        int b = 3;
 
         System.out.println("덧셈: " + (a + b));
         System.out.println("뺄셈: " + (a - b));
+        System.out.println("곱셈: " + (a * b));  // 추가
+        System.out.println("나눗셈: " + (a / b)); // 추가
     }
 }
